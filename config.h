0363b2221d375160db213880a0fd41b67373f1fb
Mahdi Mirzade <me@mahdym.ir>
2022-04-01 21:27:15 +0430

    Tweaks

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=12:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=12";
 /* Spare fonts */
 static char *font2[] = {
-        "Vazir:pixelsize=12:antialias=true:autohint=true",
-        "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+        "Vazir:pixelsize=12",
+        "JoyPixels:pixelsize=10",
 };
 static int borderpx = 2;
 
